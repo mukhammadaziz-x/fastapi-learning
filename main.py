1dbaa52db7e503b9a79d171630894dbbf1ef39eb
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-16 19:48:47 +0500

    updated

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
+async def index():
+    return {"message": "Hello World!"}
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+
 
