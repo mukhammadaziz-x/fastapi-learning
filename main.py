fe05584bda3490c80b9b56b141a34a2f2d1fe9ba
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-19 10:06:41 +0500

    Updated

--- main.py
+++ main.py
@@ -27,13 +27,14 @@ app = FastAPI()
 #     fake_db.append(user)
 #     return fake_db
 
+#----------------------------------------------------------------------------------------------------------------------#
 # Task
 # CRUD app for a little store
 # It should be able to:
-# 1. Add products
-# 2. Read products
-# 3. Update products
-# 4. Delete products
+    # 1. Add products
+    # 2. Read products
+    # 3. Update products
+    # 4. Delete products
 
 products_db = []
 
@@ -60,7 +61,8 @@ def update_product(id:int, updated_product:Product):
             product.price = updated_product.price
             product.quantity = updated_product.quantity
             return product
-    return f"Message: Doesn't have like product"
+    return f"Message: Product not found"
+
 @app.delete('/delete_product/{id}')
 def delete_product(id:int):
     for product in products_db:
