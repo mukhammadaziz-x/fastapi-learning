087dd3102fbd3422b43f402e790695095dd4745a
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-23 09:43:58 +0500

    Category and search

--- lessons/lesson2.py
+++ lessons/lesson2.py
@@ -18,7 +18,11 @@ app = FastAPI()
 # async def news_feed(search: Annotated[list[str] | None, feed_query] = "all"):
 #     return {'news': ['News1', 'News2', 'News3'], 'search_key': search}
 
-feed_query = Query(max_length=50, title="Qidiruv kaliti", description="Lorem ipsum", alias="news-query", include_in_schema=False)
-@app.get('/news/feed/')
-async def news_feed(search: Annotated[str, feed_query] = "all"):
-    return {'news': ['News1', 'News2', 'News3'], 'search_key': search}
+# feed_query = Query(max_length=50, title="Qidiruv kaliti", description="Lorem ipsum", alias="news-query", include_in_schema=False)
+# @app.get('/news/feed/')
+# async def news_feed(search: Annotated[str, feed_query] = "all"):
+#     return {'news': ['News1', 'News2', 'News3'], 'search_key': search}
+
+@app.get('/news/feed/{category}')
+async def news_feed(category: str, search: str = "all"):
+    return {'news': ['News1', 'News2', 'News3'], 'category': category, 'search_key': search}
