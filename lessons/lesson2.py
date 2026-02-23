52ee72b7cd386b306fffce5239f79ffab1c3e7d8
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-23 09:51:10 +0500

    new_id and new method - gt=0, le=1000, Grater than, Lower equal.

--- lessons/lesson2.py
+++ lessons/lesson2.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from typing import Annotated
 
 app = FastAPI()
@@ -23,6 +23,12 @@ app = FastAPI()
 # async def news_feed(search: Annotated[str, feed_query] = "all"):
 #     return {'news': ['News1', 'News2', 'News3'], 'search_key': search}
 
-@app.get('/news/feed/{category}')
-async def news_feed(category: str, search: str = "all"):
-    return {'news': ['News1', 'News2', 'News3'], 'category': category, 'search_key': search}
+# @app.get('/news/feed/{category}')
+# async def news_feed(category: str, search: str = "all"):
+#     return {'news': ['News1', 'News2', 'News3'], 'category': category, 'search_key': search}
+
+@app.get('/news/feed/{new_id}')
+async def news_feed(
+        new_id: Annotated[int, Path(title="Yangilik id", gt=0, le=1000)],
+        search: str = "all"):
+    return {'news': ['News1', 'News2', 'News3'], 'new_id': new_id, 'search_key': search}
