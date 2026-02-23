d31e2e10a818874453a604bfd232c287cb0c5852
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-23 09:05:32 +0500

    Pattern ishlatib ko'rdim, bu bilan ko'p foydali ishlar qilsa bo'lar ekan.

--- lessons/lesson2.py
+++ lessons/lesson2.py
@@ -3,7 +3,7 @@ from typing import Annotated
 
 app = FastAPI()
 
-feed_query = Query(max_length=50, min_length=5, pattern="[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+")
+feed_query = Query(max_length=50, min_length=5, pattern="[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+") # pattern yordamida emailni to'g'ri kiritish qoidasi ko'rsatilgan.
 @app.get('/news/feed/')
 async def news_feed(search: Annotated[str | None, feed_query] = None):
     return {'news': ['News1', 'News2', 'News3'], 'search_key': search}
