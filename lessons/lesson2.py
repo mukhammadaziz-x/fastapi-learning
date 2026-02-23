1a0cdf3074011a8d81dd6c7248c0ce575f62ada3
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-23 09:39:15 +0500

    title, description, alias and include_in_schema

--- lessons/lesson2.py
+++ lessons/lesson2.py
@@ -8,7 +8,17 @@ app = FastAPI()
 # async def news_feed(search: Annotated[str | None, feed_query] = None):
 #     return {'news': ['News1', 'News2', 'News3'], 'search_key': search}
 
-feed_query = Query(max_length=50, min_length=5)
+# feed_query = Query(max_length=50, min_length=5)
+# @app.get('/news/feed/')
+# async def news_feed(search: Annotated[str | None, feed_query] = "all"):
+#     return {'news': ['News1', 'News2', 'News3'], 'search_key': search}
+
+# feed_query = Query(max_length=50)
+# @app.get('/news/feed/')
+# async def news_feed(search: Annotated[list[str] | None, feed_query] = "all"):
+#     return {'news': ['News1', 'News2', 'News3'], 'search_key': search}
+
+feed_query = Query(max_length=50, title="Qidiruv kaliti", description="Lorem ipsum", alias="news-query", include_in_schema=False)
 @app.get('/news/feed/')
-async def news_feed(search: Annotated[str | None, feed_query] = "all"):
+async def news_feed(search: Annotated[str, feed_query] = "all"):
     return {'news': ['News1', 'News2', 'News3'], 'search_key': search}
