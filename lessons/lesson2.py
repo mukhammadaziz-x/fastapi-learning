d1e22d2f2857d0db69b81785d48791cff3ddd0bd
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-23 08:56:31 +0500

    Professional search qilib ko'rdik, oddiy str dan ko'ra ancha xavfsiz va mukammal filter bo'ldi. User Min va Max uzunlikda search qila olishi mumkin.

--- lessons/lesson2.py
+++ lessons/lesson2.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Annotated
 
 app = FastAPI()
 
 @app.get('/news/feed/')
-async def news_feed(search: str):
+async def news_feed(search: Annotated[str | None, Query(max_length=50, min_length=5)] = None):
     return {'news': ['News1', 'News2', 'News3'], 'search_key': search}
