3b56dad371186f8e5dd321e6f060083ceb9a28e3
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-26 09:49:31 +0500

    routers/users.py added new codes

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,10 +1,10 @@
+# app/routers/users.py
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-# from app.database import get_db
+from app.database import get_db
 
-# router = APIRouter()
-#
-# @router.get('/users/{user_id}')
-# def read_user(user_id: int, db: Session = Depends(get_db)):
-#     return db.query(User)
+router = APIRouter()
 
+@router.get("/users/{user_id}")
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    return db.query(User).filter(User.id == user_id).first()
